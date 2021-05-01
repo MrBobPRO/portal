2ba7099b106757c0f83b45237b849efd219b2ac4
MrBobPRO <boburjon_n@mail.ru>
2021-05-01 16:17:36 +0500

    Added user seearch

--- resources/views/dashboard/users/index.blade.php
+++ resources/views/dashboard/users/index.blade.php
@@ -7,6 +7,19 @@
    <link href="{{ asset('css/no_sidebar/styles.css') }}" rel="stylesheet">
 
    <section class="users-page">
+
+      {{-- Users seach start --}}
+      <div class="select2_single_container users_search_container">
+         <select class="select2_single select2_single_linked" data-placeholder="Поиск пользователей..." data-dropdown-css-class="select2_single_dropdown select2_authors_dropdown">
+            <option></option>
+            @foreach($users as $u)
+               <option value="{{ route('dashboard.users.single', $u->id)}}">{{$u->name . ' ' . $u->surname}}</option>   
+            @endforeach
+         </select>
+      </div>
+      {{-- Authors seach end --}}
+
+
       <div class="users-list">
          @foreach ($users as $u)
             <a href="{{route('dashboard.users.single', $u->id)}}" class="users-list-item">
