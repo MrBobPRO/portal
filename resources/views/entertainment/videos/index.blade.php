208ebff18b90b36adfe782f1be1c9214667c5013
MrBobPRO <boburjon_n@mail.ru>
2021-05-05 10:20:52 +0500

    videos error corrected

--- resources/views/entertainment/videos/index.blade.php
+++ resources/views/entertainment/videos/index.blade.php
@@ -9,7 +9,7 @@
          @foreach ($videos as $video)
             <div class="single-video">
                {{-- Custom id used in js --}}
-               <video class="plyr" playsinline controls id="player{{$count}}" 
+               <video class="plyr" playsinline controls id="player{{$count}}" onplay="pauseInactivePlayers({{$count}})" 
                   data-poster="/videos/entertainment/posters/{{$video->poster !='' ? $video->poster : 'default.jpg'}}">
                   <source src="/videos/entertainment/{{$video->filename}}"/>
                
